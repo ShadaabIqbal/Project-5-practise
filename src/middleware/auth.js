e3fce0d345e047146d6a8ae0c52faa602b2c9a19
ShadaabIqbal <iqbalshadaab@gmail.com>
2022-12-19 18:58:07 +0530

    done

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,21 +1,23 @@
 const jwt = require('jsonwebtoken')
 
-const authentication = async function(req, res){
-    try{
-const token = req.authorisation["Bearer token"]
-if(!token)  return res.status(401).send({status: false, message: 'Token is not present'})
+const authentication = async function (req, res, next) {
+    try {
+        const headerAuth = req.headers["authorization"]
+        const split = headerAuth.split(' ')
+        let token = split[1]
+        if (!token) return res.status(401).send({ status: false, message: 'Token is not present' })
 
-jwt.verify(token, 'group29', function(error, decodedToken){
-    if(error && error.message === 'jwt expired') return res.status(401).send({status: false, message: 'JWT is expired'})
-    if(error) return res.status(401).send({status: false, message: error.message})
-        else{
-            req.decodedToken = decodedToken
-        next()
-    }
-})
+        jwt.verify(token, 'group29', function (error, decodedToken) {
+            if (error && error.message === 'jwt expired') return res.status(401).send({ status: false, message: 'JWT is expired' })
+            if (error) return res.status(401).send({ status: false, message: error.message })
+            else {
+                req.decodedToken = decodedToken
+                next()
+            }
+        })
 
-    }catch(error){
-       return res.status(500).send({status: false, message: error.message})
+    } catch (error) {
+        return res.status(500).send({ status: false, message: error.message })
     }
 }
 
